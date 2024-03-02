c1a125e7d4c7e4e0780bca05cfc0401ae3f741b9
jlokitha <ljanindulokitha@gmail.com>
2024-03-02 17:21:44 +0530

    Dashvoard UI implemented | Memeber UI Implemented

---
+++ src/main/java/lk/ijse/library_management/controller/AdminGlobalFormController.java
@@ -0,0 +1,204 @@
+package lk.ijse.library_management.controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class AdminGlobalFormController {
+
+    @FXML
+    private JFXButton btnDashboard;
+
+    @FXML
+    private ImageView imgDashboard;
+
+    @FXML
+    private Label lblDashboard;
+
+    @FXML
+    private JFXButton btnIssued;
+
+    @FXML
+    private ImageView imgOrder;
+
+    @FXML
+    private Label lblOrder;
+
+    @FXML
+    private JFXButton btnMember;
+
+    @FXML
+    private ImageView imgCustomer;
+
+    @FXML
+    private Label lblCustomer;
+
+    @FXML
+    private JFXButton btnBranch;
+
+    @FXML
+    private ImageView imgSupplier;
+
+    @FXML
+    private Label lblSupplier;
+
+    @FXML
+    private JFXButton btnLogOut;
+
+    @FXML
+    private ImageView imgLogOut;
+
+    @FXML
+    private Label lblLogout;
+
+    @FXML
+    private JFXButton btnBook;
+
+    @FXML
+    private ImageView imgItem;
+
+    @FXML
+    private Label lblItem;
+
+    @FXML
+    private Label labelUser;
+
+    @FXML
+    private JFXTextField txtSearch;
+
+    @FXML
+    private ImageView imgProfile;
+
+    @FXML
+    private JFXButton btnProfile;
+
+    @FXML
+    private AnchorPane pagingPane;
+
+    @FXML
+    private ImageView imgGreyBack;
+
+    @FXML
+    private AnchorPane popUpPane;
+
+    @FXML
+    void btnBookOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnBranchOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnBranchOnMouseEntered(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnBranchOnMouseExited(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnDashboardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDashboardOnMouseEntered(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnDashboardOnMouseExited(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnIssuedOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnIssuedOnMouseEntered(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnIssuedOnMouseExited(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnItemOnMouseEntered(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnItemOnMouseExited(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnLogOutOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLogOutOnMouseEntered(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnLogOutOnMouseExited(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnMemberOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnMemberOnMouseEntered(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnMemberOnMouseExited(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnProfileOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnProfileOnMouseEntered(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnProfileOnMouseExited(MouseEvent event) {
+
+    }
+
+    @FXML
+    void txtSearchOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtSearchOnMouseClicked(MouseEvent event) {
+
+    }
+
+}
---
+++ src/main/java/lk/ijse/library_management/controller/MemberManageFormController.java
@@ -0,0 +1,40 @@
+package lk.ijse.library_management.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+
+public class MemberManageFormController {
+
+    @FXML
+    private VBox vBox;
+
+    @FXML
+    private JFXButton btnAdd;
+
+    @FXML
+    private ImageView imgAdd;
+
+    @FXML
+    private Label lblAdd;
+
+    @FXML
+    void btnAddCustomerOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnAddOnMouseEntered(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnAddOnMouseExited(MouseEvent event) {
+
+    }
+
+}
